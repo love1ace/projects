656c8b4ff2f0805191965aa629e1a8f95460d73a
love1ace <lovelacedud@gmail.com>
2024-09-22 12:40:23 +0900

    프로젝트 목록 수정

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -9,7 +9,7 @@ interface ProjectData {
 
 export const projects: ProjectData[] = [
     {
-        id: 14,
+        id: 12,
         name: "byul 2.0",
         description: "commit message generator by byul AI",
         logo: "./byul.png",
@@ -17,7 +17,7 @@ export const projects: ProjectData[] = [
         link: "https://www.npmjs.com/package/byul"
     },
     {
-        id: 13,
+        id: 11,
         name: "Taskl",
         description: "Task management app",
         logo: "./taskl.png",
@@ -25,7 +25,7 @@ export const projects: ProjectData[] = [
         link: "https://github.com/love1ace/taskl"
     },
     {
-        id: 12,
+        id: 10,
         name: "KIM",
         description: "Dongyeong Kim's Portfolio",
         logo: "./emoji.png",
@@ -33,7 +33,7 @@ export const projects: ProjectData[] = [
         link: "https://dongyeongkim.com/"
     },
     {
-        id: 11,
+        id: 9,
         name: "PLY",
         description: "Video link-based playlist sharing platform",
         logo: "./ply.png",
@@ -41,7 +41,7 @@ export const projects: ProjectData[] = [
         link: "https://github.com/Dev-FE-1/Toy_Project_3_Team3"
     },
     {
-        id: 10,
+        id: 8,
         name: "byul alias",
         description: "Simple git alias manager",
         logo: "./byulalias2.png",
@@ -49,7 +49,7 @@ export const projects: ProjectData[] = [
         link: "https://www.npmjs.com/package/byul-alias"
     },
     {
-        id: 9,
+        id: 7,
         name: "byulhook",
         description: "byulhook is a tool for managing Git hooks",
         logo: "./byulhook.png",
@@ -57,7 +57,7 @@ export const projects: ProjectData[] = [
         link: "https://www.npmjs.com/package/byulhook"
     },
     {
-        id: 8,
+        id: 6,
         name: "byul",
         description: "It quickly and easily formats commit",
         logo: "./byul2.png",
@@ -65,7 +65,7 @@ export const projects: ProjectData[] = [
         link: "https://www.npmjs.com/package/byul"
     },
     {
-        id: 7,
+        id: 5,
         name: "woochee",
         description: "Payroll and task management platform",
         logo: "./woochee.png",
@@ -73,7 +73,7 @@ export const projects: ProjectData[] = [
         link: "https://github.com/Dev-FE-1/woowahan-siblings"
     },
     {
-        id: 6,
+        id: 4,
         name: "Clock App",
         description: "Electron desktop clock app",
         logo: "./clock.png",
@@ -81,7 +81,7 @@ export const projects: ProjectData[] = [
         link: "https://github.com/love1ace/electron-clock"
     },
     {
-        id: 5,
+        id: 3,
         name: "Aucom",
         description: "Auto GitHub contribution graph.",
         logo: "./aucom.png",
@@ -89,28 +89,12 @@ export const projects: ProjectData[] = [
         link: "https://github.com/love1ace/Auto-commit.com"
     },
     {
-        id: 4,
+        id: 2,
         name: "Portfolio",
         description: "First portfolio site with Vanila JS",
         logo: "./kim.png",
         tags: ["2024"],
         link: "https://love1ace.github.io/DONGYEONG-KIM/en/html/index.html"
-    },
-    {
-        id: 3,
-        name: "Intranet Login",
-        description: "Intranet login form design",
-        logo: "./login.png",
-        tags: ["2024"],
-        link: "https://love1ace.github.io/intranet-login"
-    },
-    {
-        id: 2,
-        name: "Don't click",
-        description: "Don't Click This Button",
-        logo: "./dont.png",
-        tags: ["2024"],
-        link: "https://love1ace.github.io/dont-click/src/index.html"
     },
     {
         id: 1,
